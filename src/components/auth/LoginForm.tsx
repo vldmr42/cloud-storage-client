6678a026c313a42908ad1e695371236df3508cf7
vldmr42 <kmb14056@yandex.ru>
2023-11-25 11:43:39 +0400

    init

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Auth.module.scss';
+
+const LoginForm = () => {
+    return <div className={styles.formBlock}>AuthForm</div>;
+};
+
+export default LoginForm;
